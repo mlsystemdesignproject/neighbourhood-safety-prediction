bfe3242b9208357effedac4928c77a1f0e0b190d
Roman Zabolotin <rzabolotin@gmail.com>
2023-10-29 08:57:12 +1000

    update predictions update folium map add ml_flow lab

--- app/streamlit-app/london-map-folium.py
+++ app/streamlit-app/london-map-folium.py
@@ -1,5 +1,6 @@
 import datetime
-import random
+import json
+from typing import Dict
 
 import folium
 import matplotlib.colors as mcolors
@@ -8,18 +9,59 @@ import pandas as pd
 import streamlit as st
 from streamlit_folium import folium_static
 
+PREDICTIONS_FILE = "data/predictions/roman_3_predictions.csv"
+GEO_FILE = "data/london-map/london_boroughs.json"
 
-def get_data(date: datetime.date) -> pd.DataFrame:
-    return pd.DataFrame(
-        {
-            "borough": get_borough_list(),
-            "crime_prediction": [
-                random.uniform(0, 1) for _ in range(len(get_borough_list()))
-            ],
+
+@st.cache_data
+def get_data() -> pd.DataFrame:
+    df = pd.read_csv(PREDICTIONS_FILE)
+    df["month"] = pd.to_datetime(df.month)
+    df["month"] = df["month"].to_numpy().astype("datetime64[M]")
+    df["month"] = pd.to_datetime(df.month)
+    return df
+
+
+@st.cache_data
+def get_map_base() -> Dict:
+    return json.load(open(GEO_FILE, "r"))
+
+
+@st.cache_data
+def get_map_data(df: pd.DataFrame, date: datetime.date, variable: str) -> Dict:
+    base = get_map_base()
+    df = df.loc[
+        ((df.month.dt.date == date.replace(day=1)) & (df.variable == variable)),
+    ]
+    data_dict = df.set_index("borough")["value"].to_dict()
+
+    if data_dict and variable != "Rating":
+        norm = mcolors.Normalize(vmin=0, vmax=1 * max(data_dict.values()))
+    else:
+        norm = mcolors.Normalize(vmin=0, vmax=1)
+
+    for feature in base["features"]:
+        borough_name = feature["properties"]["name"]
+        value = data_dict.get(borough_name, 0)
+        cmap = plt.get_cmap("RdYlGn_r")
+
+        feature["properties"]["value"] = value
+        if variable == "Rating":
+            feature["properties"]["value_round"] = round(value * 100)
+        else:
+            feature["properties"]["value_round"] = round(value)
+        feature["properties"]["style"] = {
+            "color": "black",
+            "weight": 2,
+            "fillColor": mcolors.to_hex(cmap(norm(value))),
+            "fillOpacity": 0.7,
+            "dashArray": "5, 5",
         }
-    )
+
+    return base
 
 
+@st.cache_data
 def get_borough_list() -> list:
     return [
         "City of London",
@@ -60,72 +102,25 @@ def get_borough_list() -> list:
     ]
 
 
-def insert_map_1() -> None:
-    # Create a map centered around London
+def insert_map(df: pd.DataFrame, date: datetime.date, variable: str) -> None:
     m = folium.Map(location=[51.5074, -0.1278], zoom_start=10)
+    map_data = get_map_data(df, date, variable)
 
-    geo_data = r"data/london-map/london_boroughs.json"
+    popup = folium.GeoJsonPopup(
+        fields=["name", "value_round"],
+        aliases=["Borough", variable],
+        localize=True,
+        labels=True,
+        style="background-color: yellow;",
+    )
 
-    folium.Choropleth(
-        geo_data=geo_data,
-        data=get_data(),
-        columns=["borough", "crime_prediction"],
-        key_on="feature.properties.name",
-        fill_color="RdYlGn_r",
-        fill_opacity=0.7,
-        line_opacity=0.2,
-        legend_name="London Boroughs",
-    ).add_to(m)
-
-    # Render the map
-    folium_static(m, width=1000, height=800)
-
-
-def insert_map(date: datetime.date) -> None:
-    # Create a map centered around London
-    m = folium.Map(location=[51.5074, -0.1278], zoom_start=10)
-
-    geo_data = r"data/london-map/london_boroughs.json"
-
-    # Load your data into a DataFrame
-    data = get_data(date)
-    data_dict = data.set_index("borough")["crime_prediction"].to_dict()
-
-    # Create a function to color each borough based on crime prediction
-    def color(feature):
-        key = feature["properties"]["name"]
-        value = data_dict[key]
-        cmap = plt.get_cmap("RdYlGn_r")
-        norm = mcolors.Normalize(
-            vmin=0, vmax=1
-        )  # assuming your values range from 0 to 1
-        return mcolors.to_hex(cmap(norm(value)))
-
-    # Create a function to add a popup to each borough
-    def add_popup(feature):
-        borough = feature["properties"]["name"]
-        crime_prediction = data_dict[borough]
-        html = f"""
-            <h4>{borough}</h4><hr>
-            <h5>Crime prediction: {crime_prediction}</h5>
-        """
-        return folium.Popup(html, max_width=200)
-
-    # Add the GeoJson layer
     folium.GeoJson(
-        geo_data,
-        style_function=lambda feature: {
-            "fillColor": color(feature),
-            "color": "black",
-            "weight": 2,
-            "dashArray": "5, 5",
-            "fillOpacity": 0.7,
-        },
+        map_data,
+        style_function=lambda x: x["properties"]["style"],
         highlight_function=lambda x: {"weight": 1, "color": "black"},
-        popup=add_popup,
+        popup=popup,
     ).add_to(m)
 
-    # Render the map
     folium_static(m, width=1000, height=800)
 
 
@@ -134,19 +129,30 @@ def make_layout() -> None:
         page_title="London safety analysis",
         layout="wide",  # This sets the layout to wide mode
     )
-    # Add a title to the application
     st.title("London safety analysis")
-    # Choose date
-    col1, col2 = st.columns([10, 90])
+
+    df = get_data()
+
+    col1, col2, col3 = st.columns([10, 30, 60])
     chosen_date = col1.date_input(
         label="Choose date",
         value=datetime.date.today(),
         min_value=datetime.date.today(),
-        max_value=datetime.date.today() + datetime.timedelta(days=7),
+        max_value=datetime.date.today() + datetime.timedelta(days=365),
     )
-    col2.write("")
 
-    insert_map(chosen_date)
+    variables = list(df.variable.unique())
+    variables.remove("Rating")
+    variables.insert(0, "Rating")
+
+    chosen_variable = col2.selectbox(
+        label="Choose Variable",
+        options=variables,
+    )
+
+    col3.write("")
+
+    insert_map(df, chosen_date, chosen_variable)
 
     st.write(
         "This is a Streamlit application that displays a Folium map of London. \
